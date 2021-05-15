087bf954b58ad94173fa05340cadb673001ef661
DarkReduX <furyredux@gmail.com>
2021-05-15 16:19:19 +0300

    fix: exception TeamsInfo is exist in database, add Teams to db before adding match

--- CSGO match result prediction(курсовая практика)/Jobs/Parser.cs
+++ CSGO match result prediction(курсовая практика)/Jobs/Parser.cs
@@ -24,8 +24,11 @@ namespace CSGO_match_result_prediction_курсовая_практика_.Jobs
         {
             try
             {
-                List<MatchInfo> parseResult = ParseMatchesToday().Result;
-                foreach (var match in parseResult)
+                List<MatchInfo> matches = ParseMatchesToday().Result;
+                List<MatchInfo> needPredict = db.MatchesInfo.Where(m => m.Prediction == null)
+                    .Include(c => c.TeamsInfo)
+                    .Include(s => s.TeamsInfo.Select(sel => sel.Stats)).ToList();
+                foreach (var match in needPredict)
                 {
                     match.Prediction = PredictMatch(match).Result;
                     db.Entry(match).State = EntityState.Modified;
@@ -45,16 +48,83 @@ namespace CSGO_match_result_prediction_курсовая_практика_.Jobs
             adress = @"https://www.hltv.org/matches";
             context = BrowsingContext.New(config);
             document = await context.OpenAsync(adress);
+            const int count = 10;
             List<MatchInfo> matchInfos = new List<MatchInfo>();
+            List<TeamInfo> teamsInfo = new List<TeamInfo>();
             var upcomingMatchesList = document.All.Where(m => m.LocalName == "div" && m.ClassName == "upcomingMatch ");
             Console.WriteLine(upcomingMatchesList.Count());
             int counter = 0;
-            var dbMatchesInfo = db.MatchesInfo;
-            var dbMatchesNotLoaded = db.MatchesNotLoaded;
+            var dbMatchesInfo = db.MatchesInfo.ToList();
+            var dbMatchesNotLoaded = db.MatchesNotLoaded.ToList();
+            foreach (var item in upcomingMatchesList)
+            {
+                System.Diagnostics.Debug.WriteLine(upcomingMatchesList.Count());
+                if (counter > count)
+                    break;
+                var itemTeams = item.GetElementsByClassName("matchTeamName text-ellipsis");
+                string EventName = "Empty";
+                if (itemTeams.Count() == 2 && EventName != null)
+                {
+                    counter++;
+                    string TeamName_1, TeamName_2;
+                    //TeamInfo Team1, Team2;
+                    var itemLogos = item.GetElementsByTagName("img");
+                    var itemMatchUrl = item.GetElementsByTagName("a")[0].GetAttribute("href");
+                    TeamName_1 = itemTeams[0].TextContent;
+                    TeamName_2 = itemTeams[1].TextContent;
+                    var stats = ParseMatchStats(@"https://www.hltv.org" + itemMatchUrl).Result;
+                    string team1_id = item.GetAttribute("team1"),
+                              team2_id = item.GetAttribute("team2");
+                    //var Team1 = db.TeamsInfo
+                    //    .Where(t => t.Id == team1_id)
+                    //    .FirstOrDefault();
+                    //var Team2 = db.TeamsInfo
+                    //    .Where(t => t.Id == team2_id)
+                    //    .FirstOrDefault();
+                    //if (Team1 == null)
+                    TeamInfo Team1 = new TeamInfo
+                    {
+                        Id = team1_id,
+                        Name = TeamName_1,
+                        LogoUrl = itemLogos[0].GetAttribute("src"),
+                        Stats = stats[0]
+                    };
+                    //if (Team2 == null)
+                    TeamInfo Team2 = new TeamInfo
+                    {
+                        Id = team2_id,
+                        Name = TeamName_2,
+                        LogoUrl = itemLogos[1].GetAttribute("src"),
+                        Stats = stats[1]
+                    };
+                    var team1contains = teamsInfo
+                        .Where(t => t.Id == Team1.Id).FirstOrDefault();
+                    var team1containsDb = db.TeamsInfo.ToList()
+                        .FirstOrDefault(t => t.Id == Team1.Id);
+                    var team2contains = teamsInfo
+                        .Where(t => t.Id == Team2.Id).FirstOrDefault();
+                    var team2containsDb = db.TeamsInfo.ToList()
+                        .FirstOrDefault(t => t.Id == Team2.Id);
+                    if (team1contains == null && team1containsDb == null)
+                    {
+                        teamsInfo.Add(Team1);
+                        db.TeamsInfo.Add(Team1);
+                    }
+                    if (team2contains == null && team2containsDb == null)
+                    {
+                        teamsInfo.Add(Team2);
+                        db.TeamsInfo.Add(Team2);
+                    }
+
+                }
+            }
+            counter = 0;
+            //db.TeamsInfo.AddRange(teamsInfo);
+            db.SaveChanges();
             foreach (var item in upcomingMatchesList)
             {
 
-                if (counter > 20)
+                if (counter > count)
                     break;
                 counter++;
                 var itemEvent = item.GetElementsByClassName("matchEventName ")
@@ -79,10 +149,10 @@ namespace CSGO_match_result_prediction_курсовая_практика_.Jobs
                     var stats = ParseMatchStats("https://www.hltv.org" + itemMatchUrl).Result;
                     string team1_id = item.GetAttribute("team1"),
                            team2_id = item.GetAttribute("team2");
-                    var Team1 = db.TeamsInfo
+                    var Team1 = db.TeamsInfo.ToList()
                         .Where(t => t.Id == team1_id)
                         .FirstOrDefault();
-                    var Team2 = db.TeamsInfo
+                    var Team2 = db.TeamsInfo.ToList()
                         .Where(t => t.Id == team2_id)
                         .FirstOrDefault();
                     if (Team1 == null)
@@ -93,6 +163,11 @@ namespace CSGO_match_result_prediction_курсовая_практика_.Jobs
                             LogoUrl = itemLogos[0].GetAttribute("src"),
                             Stats = stats[0]
                         };
+                    else
+                    {
+                        Team1.Stats = stats[0];
+                        db.Entry(Team1).State = EntityState.Modified;
+                    }
                     if (Team2 == null)
                         Team2 = new TeamInfo
                         {
@@ -101,6 +176,11 @@ namespace CSGO_match_result_prediction_курсовая_практика_.Jobs
                             LogoUrl = itemLogos[1].GetAttribute("src"),
                             Stats = stats[1]
                         };
+                    else
+                    {
+                        Team2.Stats = stats[0];
+                        db.Entry(Team2).State = EntityState.Modified;
+                    }
                     MatchInfo match = new MatchInfo
                     {
                         Id = Guid.NewGuid(),
@@ -180,10 +260,13 @@ namespace CSGO_match_result_prediction_курсовая_практика_.Jobs
             //.ThenInclude();
             foreach (var match in matches)
             {
+                if (match.TeamsInfo.Count() != 2)
+                    continue;
                 adress = "https://www.hltv.org" + match.MatchUrl;
                 document = await context.OpenAsync(adress);
                 if (document.All.
-                    Where(m => m.ClassName == "won" && m.LocalName == "div").Count() > 0)
+                    Where(m => m.ClassName == "won" && m.LocalName == "div")
+                    .Count() > 0)
                 {
                     var team1_score = document.GetElementsByClassName("team1-gradient")[0];
                     var team2_score = document.GetElementsByClassName("team2-gradient")[0];
@@ -216,35 +299,40 @@ namespace CSGO_match_result_prediction_курсовая_практика_.Jobs
                 }
             }
         }
-        public Task<TeamInfo> PredictMatch(MatchInfo match)
+        public async Task<TeamInfo> PredictMatch(MatchInfo match)
         {
-            TeamInfo team1, team2;
-            double team1_chance = 0, team2_chance = 0;
-
-            team1 = match.TeamsInfo.ToList()[0];
-            team2 = match.TeamsInfo.ToList()[1];
-            var team1_stats = team1.Stats.ToList();
-            var team2_stats = team2.Stats.ToList();
-
-            for (int i = 0; i < team1.Stats.Count; i++)
+            if (match.TeamsInfo.Count() == 2)
             {
-                if (team1_stats[i].WinPercentage > 70 && team1_stats[i].MapPlayed > 10)
-                    team1_chance += 1.5;
-                else if (team1_stats[i].WinPercentage >= 50)
-                    team1_chance += 1 * 0.3;
-                if (team2_stats[i].WinPercentage > 70 && team2_stats[i].MapPlayed > 10)
-                    team2_chance += 1.5;
-                else if (team1_stats[i].WinPercentage >= 50)
-                    team2_chance += 1 * 0.3;
-                /////////////////////////
-                if (team1_stats[i].WinPercentage > team2_stats[i].WinPercentage
-                && team1_stats[i].MapPlayed > team2_stats[i].MapPlayed)
-                    team1_chance += 0.2;
-                else
-                    team2_chance += 0.2;
-                ;
+                TeamInfo team1, team2;
+                double team1_chance = 0, team2_chance = 0;
+
+                team1 = match.TeamsInfo.ToList()[0];
+                team2 = match.TeamsInfo.ToList()[1];
+                var team1_stats = team1.Stats.ToList();
+                var team2_stats = team2.Stats.ToList();
+                var length = team1_stats.Count() >= team2_stats.Count() ?
+                    team2_stats.Count() : team1_stats.Count();
+                for (int i = 0; i < length; i++)
+                {
+                    if (team1_stats[i].WinPercentage > 70 && team1_stats[i].MapPlayed > 10)
+                        team1_chance += 1.5;
+                    else if (team1_stats[i].WinPercentage >= 50)
+                        team1_chance += 1 * 0.3;
+                    if (team2_stats[i].WinPercentage > 70 && team2_stats[i].MapPlayed > 10)
+                        team2_chance += 1.5;
+                    else if (team1_stats[i].WinPercentage >= 50)
+                        team2_chance += 1 * 0.3;
+                    /////////////////////////
+                    if (team1_stats[i].WinPercentage > team2_stats[i].WinPercentage
+                    && team1_stats[i].MapPlayed > team2_stats[i].MapPlayed)
+                        team1_chance += 0.2;
+                    else
+                        team2_chance += 0.2;
+                    ;
+                }
+                return team1_chance >= team2_chance ? team1 : team2;
             }
-            return team1_chance >= team2_chance ? team1 : team2;
+            return null;
         }
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
